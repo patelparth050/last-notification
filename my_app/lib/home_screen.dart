0ee35a2b8f0cf45af7278a4f5f2d7912cbad0ffa
Parth Patel <patelparth292906@gmail.com>
2023-07-03 19:46:55 +0530

    Initial Commit

---
+++ my_app/lib/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/notification_services.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+NotificationServices notificationServices = NotificationServices();
+
+
+  @override
+  void initState(){
+    super.initState();
+    notificationServices.requestNotificationPermission();
+    // notificationServices.forgroundMessage();
+    notificationServices.firebaseInit(context);
+    // notificationServices.setupInteractMessage(context);
+    // notificationServices.isTokenRefresh();
+    notificationServices.getDeviceToken().then((value) {
+      print('device token');
+      print(value);
+    });
+    }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold();
+  }
+}
